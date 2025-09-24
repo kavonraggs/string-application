f65e19d2a050e0bc9c7303d659dde333f24958e1
kavonraggs <kavonraggs@gmail.com>
2025-09-24 10:34:38 -0500

    Created Receipt and String app

---
+++ src/com/pluralsight/VariableApp.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String color = "blue";
+        System.out.println("My favorite color is " + color);
+
+        int year = 2025;
+        System.out.println("I started this class in " + year);
+
+        char middleInitial = 'M';
+        System.out.println("My middle initial is " + middleInitial);
+
+        boolean hasPets = true ;
+        System.out.println("It is " + hasPets + " that I have pets");
+
+        String message = "Bye! Take care!";
+        System.out.println(message);
+
+        // exercise 2
+        int daysOfWeek = 7;
+        System.out.println("There are " + daysOfWeek + "in the week");
+
+        double coffeePrice = 4.99;
+        System.out.println("The price of the coffee is $" + coffeePrice);
+
+        char favLetter = 'K';
+        System.out.println("My favorite letter is " + favLetter);
+
+        boolean isRaining = true;
+        System.out.println("It is " + isRaining + "that it is raining");
+
+
+
+
+    }
+}
